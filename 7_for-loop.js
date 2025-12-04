b83507fd5a38a6fcb776ff45198a23ec4fc3e815
nadim-bs <nadim.hossain@brainstation-23.com>
2025-12-05 01:54:37 +0600

    loop

---
+++ 7_for-loop.js
@@ -0,0 +1,20 @@
+// for loop
+for (let x = 1; x<=10; x++){
+    // console.log(x);
+    console.log("javaScript")
+    console.log 
+}
+
+// while loop
+let i = 0;
+while(i<10){
+    console.log(i)
+    i++;
+}
+
+// do while
+let a =0;
+do{
+    console.log(a)
+    a++;
+}while(a<10)
