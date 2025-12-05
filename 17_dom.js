72d861d28619338ddcff9458ec7e138f07f53bc5
nadim-bs <nadim.hossain@brainstation-23.com>
2025-12-05 16:04:32 +0600

    dom introduction

---
+++ 17_dom.js
@@ -0,0 +1,10 @@
+// by id
+document.getElementById("heading1").innerHTML="Hi"
+
+// by tag name
+document.getElementsByTagName("h1")[1].innerHTML ="BYE"
+
+// by class name
+document.getElementsByClassName("para1")[0].innerHTML="Good luck"
+
+// querySelector: same as get elements. but for id we have to use #, for class use ., for tag use tag name
