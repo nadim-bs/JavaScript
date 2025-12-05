651eb415ad07a8ba1aa3c8a112e051f68cc91231
nadim-bs <nadim.hossain@brainstation-23.com>
2025-12-05 18:44:42 +0600

    button click-onclick

---
+++ 18_buttonClick.js
@@ -0,0 +1,14 @@
+function display(){
+            alert("kire")
+        }
+
+let msg = document.querySelector("#paraId")
+function msg1(){
+    msg.innerHTML="You have click button 1"
+
+}
+
+function msg2(){
+     msg.innerHTML="You have click button 2"
+
+}
